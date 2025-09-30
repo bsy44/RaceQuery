b18a957a6993277fe263d4b1aba5c84f500fbf28
bsy44 <syboubacar444@gmail.com>
2025-10-01 00:52:24 +0200

    initialisation du front avec angular

--- app.py
+++ app.py
@@ -18,16 +18,16 @@ app.register_blueprint(driver_bp, url_prefix='/drivers')
 app.register_blueprint(contructor_bp, url_prefix='/constructors')
 app.register_blueprint(circuit_bp, url_prefix='/circuits')
 app.register_blueprint(race_bp, url_prefix='/race')
-app.register_blueprint(qualifying_bp, url_prefix='/qualifyng')
+app.register_blueprint(qualifying_bp, url_prefix='/qualifying')
 app.register_blueprint(sprint_bp, url_prefix='/sprint')
 app.register_blueprint(lap_bp, url_prefix='/lap')
 app.register_blueprint(result_bp, url_prefix='/results')
 app.register_blueprint(driver_standing_bp, url_prefix='/drivers-standings')
 app.register_blueprint(constructor_standing_bp, url_prefix='/constructors-standings')
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/f1')
+def hello():
+    return {"message": "Hello from flask"}
 
 if __name__ == "__main__":
     app.run(debug=True)
