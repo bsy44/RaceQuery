13904af5cc6afeef51d142838cea8e490b81408a
bsy44 <syboubacar444@gmail.com>
2025-12-23 00:39:32 +0100

    feat(app): ajout de la route prod en environnement

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from drivers.driver_controller import driver_bp
@@ -6,7 +8,15 @@ from races.race_controller import race_bp
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+env_origins = os.environ.get('ALLOWED_ORIGINS')
+
+if env_origins:
+    allowed_origins = [origin.strip() for origin in env_origins.split(',')]
+else:
+    allowed_origins = ["http://localhost:4200"]
+
+CORS(app, resources={r"/*": {"origins": allowed_origins}})
 
 app.register_blueprint(driver_bp)
 app.register_blueprint(team_bp)
@@ -14,4 +24,7 @@ app.register_blueprint(race_bp)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    is_debug = os.environ.get('FLASK_ENV') == 'development'
+
+    app.run(debug=is_debug, host='0.0.0.0', port=port)
